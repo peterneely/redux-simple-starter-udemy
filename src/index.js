5a8756a1588428d7d6966e375842fe80f192d67f
Peter Neely <pgneely@gmail.com>
2016-05-08 16:25:54 -0700

    Up to 1.4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const App = function() {
-  return <div > Hi! < /div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search-bar';
+
+const API_KEY = 'AIzaSyAcHXqlzROnAQkD2anN84jhR1uzbeRP5ps';
+
+const App = () => {
+  return (
+   <div>
+    <SearchBar />
+   </div>
+  );
 }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
