62a9252a29cdd988055e48d5ba54367b001d555e
Peter Neely <pgneely@gmail.com>
2016-05-09 14:44:27 -0700

    2.33 Delayed search (debounce)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
 import VideoDetail from './components/video-detail'
+
 const API_KEY = 'AIzaSyAcHXqlzROnAQkD2anN84jhR1uzbeRP5ps';
 
 class App extends Component {
@@ -15,7 +17,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -24,9 +30,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
       </div>
