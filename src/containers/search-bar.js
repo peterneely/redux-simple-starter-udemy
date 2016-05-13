787d705d9e0eed49a44820ad94af3691ba29880f
Peter Neely <pgneely@gmail.com>
2016-05-13 14:52:08 -0700

    Initial weather app stuff

---
+++ src/containers/search-bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return(
+      <form action="" className="input-group">
+        <input type="text"/>
+      </form>
+    );
+  }
+}
