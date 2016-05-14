8fcde3b75be31c99dca56ea71f1ebba77be09ae8
Peter Neely <pgneely@gmail.com>
2016-05-14 13:36:49 -0700

    Making API call but not updating state yet

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = 'e85d6b5994040a47b16fc6b50a405f67';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
