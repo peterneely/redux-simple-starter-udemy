14e3419ff42cc505af02634a38220684a0a612d1
Peter Neely <pgneely@gmail.com>
2016-05-16 20:05:42 -0700

    const working on reducer

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,8 +1,8 @@
-import FETCH_WEATHER from '../actions/index';
+import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
   switch(action.type) {
-    case 'FETCH_WEATHER':
+    case FETCH_WEATHER:
       return [action.payload.data, ...state];
   }
   return state;
