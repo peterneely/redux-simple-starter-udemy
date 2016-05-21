8727fcb551dbae49f425fe614ea537c37a3841e9
Peter Neely <pgneely@gmail.com>
2016-05-21 08:06:18 -0700

    Boiler plate for creating new branches

--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search-bar';
-import VideoList from './components/video-list';
-import VideoDetail from './components/video-detail'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const API_KEY = 'AIzaSyAcHXqlzROnAQkD2anN84jhR1uzbeRP5ps';
+import App from './components/app';
+import reducers from './reducers';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    };
-
-    this.videoSearch('surfboards');
-  }
-
-  videoSearch(term) {
-    YTSearch({ key: API_KEY, term: term }, videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-    });
-  }
-
-  render() {
-    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
-
-    return(
-      <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
