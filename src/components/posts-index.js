fac12963bd799105af586c5a115921216cc3e8a6
Peter Neely <pgneely@gmail.com>
2016-05-22 20:05:23 -0700

    WIP fetching data on componentWillMount

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return (
-    <div>List of blog posts.</div>
-  );
-};
+class PostsIndex extends Component {
+
+  componentWillMount() {
+    console.log('fetch');
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+export default PostsIndex;
