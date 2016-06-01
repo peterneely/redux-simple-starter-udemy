17cb60797005e218dfd715b368bf7281e9ac93e6
Peter Neely <pgneely@gmail.com>
2016-05-31 17:01:31 -0700

    redux-form with action creator to post form field values

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
     return(
-      <div>Create form</div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create a new post</h3>
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="">Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="">Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
