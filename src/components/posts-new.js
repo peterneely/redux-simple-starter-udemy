2d8bba3785980d7ab2274ab4396ac2beefa47a57
Peter Neely <pgneely@gmail.com>
2016-05-31 21:22:32 -0700

    Make class methods private, using symbols

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,30 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
+const getErrorText = Symbol('getErrorText');
+const getValidationClass = Symbol('getValidationClass');
+const onSubmit = Symbol('onSubmit');
+
 class PostsNew extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
-    return(
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+    return (
+      <form onSubmit={handleSubmit(this[onSubmit].bind(this))}>
         <h3>Create a new post</h3>
-        <div className={setClassName(title)}>
+        <div className={`form-group ${this[getValidationClass](title)}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
-          <div className="text-help">{showError(title)}</div>
+          <div className="text-help">{this[getErrorText](title)}</div>
         </div>
 
-        <div className={setClassName(categories)}>
+        <div className={`form-group ${this[getValidationClass](categories)}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
-          <div className="text-help">{showError(categories)}</div>
+          <div className="text-help">{this[getErrorText](categories)}</div>
         </div>
 
-        <div className={setClassName(content)}>
+        <div className={`form-group ${this[getValidationClass](content)}`}>
           <label>Content</label>
           <textarea className="form-control" {...content} />
-          <div className="text-help">{showError(content)}</div>
+          <div className="text-help">{this[getErrorText](content)}</div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -32,30 +41,18 @@ class PostsNew extends Component {
       </form>
     );
   }
-}
 
-function setClassName(control) {
-  return `form-group ${control.touched && control.invalid ? 'has-danger' : ''}`;
-}
-
-function showError(control) {
-  return control.touched ? control.error : '';
-}
-
-function validate(values) {
-  const errors = {};
-
-  if(!values.title) {
-    errors.title = 'Enter a username';
-  }
-  if(!values.categories) {
-    errors.categories = 'Enter categories';
-  }
-  if(!values.content) {
-    errors.content = 'Enter some content';
+  [getErrorText](control) {
+    return control.touched ? control.error : '';
   }
 
-  return errors;
+  [getValidationClass](control) {
+    return control.touched && control.invalid ? 'has-danger' : '';
+  }
+
+  [onSubmit](props) {
+    this.props.createPost(props).then(() => { this.context.router.push('/') });
+  }
 }
 
 export default reduxForm({
@@ -63,3 +60,19 @@ export default reduxForm({
   fields: ['title', 'categories', 'content'],
   validate
 }, null, { createPost })(PostsNew);
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content';
+  }
+
+  return errors;
+}
