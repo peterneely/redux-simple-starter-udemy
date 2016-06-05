868cca37426cb5fbb9d51c95d598817d4791fa5c
Peter Neely <pgneely@gmail.com>
2016-06-05 10:03:27 -0700

    List of clickable posts

--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory} routes={routes} />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector('.container')
+);
